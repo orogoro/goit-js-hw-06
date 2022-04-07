776b3324ac3a6a054edb204a223719a9fa94151d
Yurii Dvornichenko <raen98@live.ru>
2022-04-08 00:01:28 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,34 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+/// Сделал вместе с Гуглом, очень было интересно решить задачу
+
+const refs = {
+  createBtn: document.querySelector("[data-create]"),
+  destroyBtn: document.querySelector("[data-destroy]"),
+};
+
+refs.createBtn.addEventListener("click", getAmount);
+refs.destroyBtn.addEventListener("click", destroyBoxes);
+
+function getAmount() {
+  let amount = +document.querySelector("#controls input").value;
+  createBoxes(amount);
+}
+
+function createBoxes(amount) {
+  let basicSize = 30;
+  let fragment = document.createDocumentFragment();
+  for (let i = 0; i < amount; i++) {
+    let size = basicSize + i * 10;
+    let div = document.createElement("div");
+    div.style.cssText = `width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()}`;
+    fragment.append(div);
+  }
+  document.querySelector("#boxes").append(fragment);
+}
+
+function destroyBoxes() {
+  document.querySelector("#boxes").innerHTML = "";
+}
