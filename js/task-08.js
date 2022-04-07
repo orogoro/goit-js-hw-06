777fb5f6f58cb42dda73796ea3d0c28f600b69cf
Yurii Dvornichenko <raen98@live.ru>
2022-04-07 19:09:37 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Проверьте еще раз, вы что-то пропустили");
+  }
+
+  //   const database = {
+  //     email: email.value,
+  //     password: password.value,
+  //   };
+  //   console.log(database);
+
+  const formData = new FormData(event.currentTarget);
+  formData.forEach((name, value) => {
+    console.log(value);
+    console.log(name);
+  });
+
+  event.currentTarget.reset();
+}
